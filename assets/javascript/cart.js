346d539d80292a80ad46120f1510f37be4e4bdea
Harsh Yadav <harsh.yadav@w3villa.com>
2023-03-22 15:32:26 +0530

    Improved pagination

--- assets/javascript/cart.js
+++ assets/javascript/cart.js
@@ -12,17 +12,20 @@ const cart = async (element) => {
           if (element.id == cart1[i].id) {
             cart1[i].quantity += 1;
             m++;
+            alert("Item added to cart");
             break;
           }
         }
         if (m == 0) {
           cart1.push(data.items[i]);
           data.items[i].quantity += 1;
+          alert("Item added to cart");
         }
         break;
       } else {
         cart1.push(data.items[i]);
         data.items[i].quantity += 1;
+        alert("Item added to cart");
         break;
       }
     }
@@ -40,4 +43,94 @@ const cart = async (element) => {
     "total-items"
   ).innerText = `${cart1.length} item(s)-Rs.${totalprice}`;
   document.getElementById("cart-heading").style.display = "block";
+};
+
+const loadcart = async () => {
+  if (localStorage.getItem("cart1")) {
+    cart1 = JSON.parse(localStorage.getItem("cart1"));
+    let html = "";
+    for (let i = 0; i < cart1.length; i++) {
+      html += `
+      <div class="${cart1[i].item_class} item">
+      <div class="item-image">
+        <img src="${cart1[i].item_image}" alt="#">
+      </div>
+      <div class="item-hero ptbs">
+        <div class="row-1 ptbs">
+          <h2>${cart1[i].item_name}</h2>
+          <p>
+            <i class="fa-light fa-indian-rupee-sign"></i>
+            ${cart1[i].item_price}
+          </p>
+        </div>
+        <div class="row-2 ptbs">
+          <span class="input-btn">
+            <input type="number"  placeholder="${cart1[i].quantity}"/>
+            <button id="${cart1[i].id}" onclick="remove(this)">Remove from Cart</button>
+          </span>
+          <span class="icons">
+            <i class="fa-light fa-heart heart"></i>
+            <i class="fa-thin fa-arrow-right-arrow-left arrow"></i>
+          </span>
+        </div>
+      </div>
+      <div class="item-footer ptbs">
+        <div class="buy-now">
+          <a href="#">
+            <i class="fa-regular fa-cart-shopping"></i>
+            Buy now
+          </a>
+        </div>
+        <div class="question">
+          <a href="#">
+            <i class="fa-light fa-circle-question"></i>
+            Question
+          </a>
+        </div>
+      </div>
+    </div>
+      `;
+    }
+    document.getElementById("cart-block").innerHTML = html;
+    document.getElementById("cart-block").style.display = "flex";
+    if (html != "") {
+      document.getElementById("hero").style.display = "none";
+      document.getElementById("grey").style.display = "none";
+      document.getElementById("why").style.display = "none";
+      document.getElementById("featured1").style.display = "none";
+      document.getElementById("featured2").style.display = "none";
+      document.getElementById("shop").style.display = "none";
+      document.getElementById("gallery").style.display = "none";
+      document.getElementById("blog").style.display = "none";
+      document.getElementById("people").style.display = "none";
+      document.getElementById("viewed").style.display = "none";
+      document.getElementById("footer-top").style.display = "none";
+    } else {
+      alert("Cart is empty");
+      window.location.assign("index.html");
+    }
+  }
+};
+
+const remove = async (element) => {
+  if (localStorage.getItem("cart1")) {
+    cart1 = JSON.parse(localStorage.getItem("cart1"));
+    for (let i = 0; i < cart1.length; i++) {
+      if (cart1[i].id == element.id) {
+        totalprice = JSON.parse(localStorage.getItem("totalprice"));
+        totalprice -= Number(cart1[i].quantity) * Number(cart1[i].item_price);
+        cart1.splice(i, 1);
+        break;
+      }
+    }
+    localStorage.setItem("cart1", JSON.stringify(cart1));
+    document.getElementById(
+      "cart-heading"
+    ).innerHTML = `<i class="fa-solid fa-${cart1.length}"></i>`;
+    document.getElementById(
+      "total-items"
+    ).innerText = `${cart1.length} item(s)-Rs.${totalprice}`;
+    localStorage.setItem("totalprice", JSON.stringify(totalprice));
+    loadcart();
+  }
 };
--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -1,61 +1,85 @@
-const search = async () => {
+const search = async (elem) => {
   const response = await fetch("./assets/data/items.json");
   const data = await response.json();
   const search = document.getElementById("search-icon").value;
+  console.log(search);
   let value = Number(
     document.getElementsByClassName("p-active")[0].getAttribute("value")
   );
   let a = value * 4 - 4;
   let html = "";
+  let arr = [];
   data.items.forEach((element) => {
-    if (element.item_name.toLowerCase().match(search.toLowerCase())) {
-      if ((element.id <= value * 4) & (element.id > a)) {
-        html += `
-            <div class="${element.item_class} item">
-        <div class="item-image">
-          <img src="${element.item_image}" alt="#">
-        </div>
-        <div class="item-hero ptbs">
-          <div class="row-1 ptbs">
-            <h2>${element.item_name}</h2>
-            <p>
-              <i class="fa-light fa-indian-rupee-sign"></i>
-              ${element.item_price}
-            </p>
-          </div>
-          <div class="row-2 ptbs">
-            <span class="input-btn">
-              <input type="number" />
-              <button>Add to Cart</button>
-            </span>
-            <span class="icons">
-              <i class="fa-light fa-heart heart"></i>
-              <i class="fa-thin fa-arrow-right-arrow-left arrow"></i>
-            </span>
-          </div>
-        </div>
-        <div class="item-footer ptbs">
-          <div class="buy-now">
-            <a href="#">
-              <i class="fa-regular fa-cart-shopping"></i>
-              Buy now
-            </a>
-          </div>
-          <div class="question">
-            <a href="#">
-              <i class="fa-light fa-circle-question"></i>
-              Question
-            </a>
-          </div>
-        </div>
-      </div>
-            `;
-      }
+    if (element.item_name.toLowerCase().match(search.toLowerCase()) && search) {
+      arr.push(element);
     }
   });
+  if(elem.id == "search-icon"){
+    setpagination(arr);
+  }
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+    if (i < value * 4 && i >= a) {
+      html += `
+      <div class="${arr[i].item_class} item">
+      <div class="item-image">
+        <img src="${arr[i].item_image}" alt="#">
+      </div>
+      <div class="item-hero ptbs">
+        <div class="row-1 ptbs">
+          <h2>${arr[i].item_name}</h2>
+          <p>
+            <i class="fa-light fa-indian-rupee-sign"></i>
+            ${arr[i].item_price}
+          </p>
+        </div>
+        <div class="row-2 ptbs">
+          <span class="input-btn">
+            <input type="number" />
+            <button>Add to Cart</button>
+            </span>
+            <span class="icons">
+            <i class="fa-light fa-heart heart"></i>
+            <i class="fa-thin fa-arrow-right-arrow-left arrow"></i>
+            </span>
+            </div>
+            </div>
+            <div class="item-footer ptbs">
+            <div class="buy-now">
+            <a href="#">
+            <i class="fa-regular fa-cart-shopping"></i>
+            Buy now
+            </a>
+            </div>
+            <div class="question">
+            <a href="#">
+            <i class="fa-light fa-circle-question"></i>
+            Question
+            </a>
+            </div>
+            </div>
+            </div>
+            `;
+    }
+  }
+  
   document.getElementById("search-block").innerHTML = html;
   document.getElementById("pagination").style.display = "flex";
+
   if (html != "") {
+    document.getElementById("search-block").style.display = "flex";
+    document.getElementById("hero").style.display = "none";
+    document.getElementById("grey").style.display = "none";
+    document.getElementById("why").style.display = "none";
+    document.getElementById("featured1").style.display = "none";
+    document.getElementById("featured2").style.display = "none";
+    document.getElementById("shop").style.display = "none";
+    document.getElementById("gallery").style.display = "none";
+    document.getElementById("blog").style.display = "none";
+    document.getElementById("people").style.display = "none";
+    document.getElementById("viewed").style.display = "none";
+    document.getElementById("footer-top").style.display = "none";
+  } else {
     document.getElementById("hero").style.display = "none";
     document.getElementById("grey").style.display = "none";
     document.getElementById("why").style.display = "none";
@@ -69,3 +93,16 @@ const search = async () => {
     document.getElementById("footer-top").style.display = "none";
   }
 };
+
+const setpagination = (arr) =>{
+  let pagination_html = ""
+  for (let j = 0; j < (arr.length/4); j++) {
+    if(j == 0){
+      pagination_html += `<li class="link p-active" value="${j+1}" onclick="pActive(this)">${j+1}</li>`;
+    }
+    else{
+      pagination_html += `<li class="link" value="${j+1}" onclick="pActive(this)">${j+1}</li>`;
+    }
+  }
+  document.getElementById("pagination-list").innerHTML = pagination_html
+}
